19e5e2edc232c7042a88a55f1eba047dae8a9d56
Savvin Sergey <savvinsergey.web@gmail.com>
2016-11-23 23:43:26 +0300

    A little bit app.js refactoring

--- app/app.js
+++ app/app.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 import Main from './components/main.react';
 
-ReactDOM.render(<Main/>,document.getElementById('container'));
+React.render(<Main/>,document.body);
