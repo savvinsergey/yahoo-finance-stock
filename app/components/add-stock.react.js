b1c41075d44876bdb7e95f1c599af09eb2236e5b
Savvin Sergey <savvinsergey.web@gmail.com>
2016-11-24 11:39:24 +0300

    Added 'addStock' component'

---
+++ app/components/add-stock.react.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class AddStock extends React.Component {
+
+    render(){
+        return (
+            <div>
+                <input type="text"/>
+                <button class="btn btn-default">Add stock</button>
+            </div>
+        );
+    }
+
+}
--- app/components/main.react.js
+++ app/components/main.react.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import StockChart from './stock-chart.react';
+import AddStock from './add-stock.react';
 
 export default class Main extends React.Component {
 
     render(){
         return (
-            <StockChart/>
+            <div>
+                <StockChart/>
+                <AddStock/>
+            </div>
         ); 
     }
     
