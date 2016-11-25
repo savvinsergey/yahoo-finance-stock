f91530b23c51e4288cee0f8ee4a979025704a1bc
Savvin Sergey <savvinsergey.web@gmail.com>
2016-11-25 14:42:24 +0300

    Added container for 'Main' component + Added ReactDOM

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Main from './components/main.react';
 
-React.render(<Main/>,document.body);
+ReactDOM.render(<Main/>,document.getElementById("container"));
